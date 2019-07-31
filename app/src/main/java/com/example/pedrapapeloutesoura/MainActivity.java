52a3e1dcd7669d17ec51502691d7e42b797cae79
Michelle <mia_araujo92@hotmail.com>
2019-07-31 12:06:48 -0300

    Projeto finalizado

---
+++ app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.pedrapapeloutesoura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view){
+        this.opcaoSelecionada("pedra");
+
+    }
+
+    public void selecionarPapel(View view){
+        this.opcaoSelecionada("papel");
+
+    }
+
+    public void selecionarTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+
+    }
+
+    public void opcaoSelecionada (String escolhaUsuario){
+
+        ImageView imagemResultado = findViewById(R.id.imageResultado);
+
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        int numero = new Random().nextInt(3);
+        String escolhaApp = opcoes[numero];
+
+        switch(escolhaApp){
+            case "pedra":
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+
+
+
+        }
+    }
+}
